bc0ac8f6bf0f9749df153e864e74ea25a0576158
María Mora <lilamandarina@hotmail.com>
2022-03-24 10:12:28 +0100

    finished

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // Happy coding!
 
+let circulosColores = document.querySelector(".canvas");
+circulosColores.addEventListener("click", function(event){
+    console.log(event.target.id);
+   
+    document.querySelector("body").style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+
+
+
+
+    
+})
+
